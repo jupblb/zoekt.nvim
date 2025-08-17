e62571dfa6ae76b7752059acfedc4257f1829867
jupblb <michal.kielbowicz@sourcegraph.com>
2025-08-17 12:37:52 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url     = "github:NixOS/nixpkgs/release-25.05";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            lua-language-server pandoc pre-commit stylua universal-ctags zoekt
+          ];
+
+          NVIM_LAZYDEV = "${pkgs.vimPlugins.lazydev-nvim}";
+        };
+      }
+    );
+}
